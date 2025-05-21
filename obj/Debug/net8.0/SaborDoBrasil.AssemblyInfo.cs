806f1f4e2029bcd1a208e30a7ebcdb1e9fcbf3fc
Sandyla Stocler <sanystocler@gmail.com>
2025-05-21 13:58:17 -0300

    Update

--- obj/Debug/net8.0/SaborDoBrasil.AssemblyInfo.cs
+++ obj/Debug/net8.0/SaborDoBrasil.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaborDoBrasil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a5fbfba982e6b219e727df8a09a2936d29f21f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2f62ed77c90af8100bfb377368da941ba7fcd53")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaborDoBrasil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaborDoBrasil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
